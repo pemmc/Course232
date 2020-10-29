34701daa723bc871278dd92ad6ebef376b6b162d
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-29 17:43:46 -0300

    Acessando .csv, montando um LIST e fazendo uma consulta LINQ com expressão LAMBDA

---
+++ Course232/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using Course232.Entities;
+
+namespace Course232
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        //windows C:\Users\<YourUserName>
+        //string sourcePath = "c:\\Users\\Administrator\\Projects\\Exercicio\\Files\\csv\\Estoque.csv";
+        //ou simplesmente usar o @ na frente antes das pastas... daí nao precisa duas barras \\
+        //string sourcePath = @"c:\Users\Administrator\Projects\Exercicio\Files\csv\Estoque.csv";
+
+        //mac
+        //diretorio padrao '/Users/nxgames/Exercicio/Files/bin/Debug/netcoreapp3.1
+        //Console.WriteLine(Directory.GetCurrentDirectory());
+
+        //https://www.udemy.com/course/programacao-orientada-a-objetos-csharp/learn/lecture/11443358#overview
+        Console.WriteLine("EXERCÍCIO DE FIXAÇÃ0");
+        Console.WriteLine("====================");
+        Console.WriteLine();
+
+        //Diretorio que coloquei o arquivo
+        string pathSeparator = Path.DirectorySeparatorChar.ToString();
+
+        string path = "/Users/nxgames/Projects/Course232/Course232";
+        string sourceFile = "csv" + pathSeparator + "Estoque.csv";
+
+        string sourceFilePath = path + pathSeparator + sourceFile;
+
+        Console.Write("Confirme o path: " + sourceFilePath);
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(sourceFilePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("C", new CultureInfo("pt-BR")));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
